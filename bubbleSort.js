0beeba3221de7d91b6bba50c02534a3f1c883577
ethan <ethanmannette@gmail.com>
2015-08-25 13:01:56 -0600

    Sorting function works. Jasmine tests fail

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,5 +1,17 @@
-function bubbleSort(arr){
+function bubbleSort (arr) {
+  var cached;
 
+  for (var x = 0; x < arr.length; x++) {
+    for (var i = 0; i < arr.length - 1; i++) {
+      if (arr[i + 1] < arr[i]) {
+        // swap
+        cached = arr[i + 1];
+        arr[i + 1] = arr[i];
+        arr[i] = cached;
+        // console.log(arr);
+      }
+    }
+  }
 }
 
 module.exports = bubbleSort;
